56e87584598714dc41ada9c90020c04714c3d169
Dennis Pomaquero <Dennis@Denniss-MacBook-Air.local>
2018-05-29 12:03:12 -0400

    added .gitignore .gitattributes

--- Tippo/ViewController.swift
+++ Tippo/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Tippo
 //
-//  Created by Dennis Pomaquero on 5/26/18.
+//  Created by Ji Hye Ha on 5/26/18.
 //  Copyright © 2018 Ji Hye Ha. All rights reserved.
 //
 
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func onTap(sender: AnyObject){
+        view.endEditing(true)
+    }
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentages = [0.15, 0.18, 0.20]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        tipLabel.text = String(format: "%.2f", tip)
+        totalLabel.text = String(format: "%.2f", total)
+    }
+    
 }
-
